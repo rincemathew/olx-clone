9f0ccae5f877fc680c3b9eae8f01a89775ba4a84
Rince Mathew <rincemathewmanattu@gmail.com>
2023-11-30 18:20:37 +0530

    create from scrach

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './olx-logo.png';
+import logo from './logo.svg';
 import './App.css';
-import Signup from './Pages/Signup';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route path='/signup' element={<Signup />}/>
-      </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {FirebaseContext} from './store/FirebaseContext';
-import firebase from './firebase/config';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <FirebaseContext.Provider value={{firebase}}>
-
-        <App />
-    </FirebaseContext.Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
